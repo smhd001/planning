06d521fd0aeca6d546b3c8208efdef030dd1d1ee
mohammad hashemy <smhd.hashemy@outlook.com>
2024-05-16 16:43:08 +0330

    add some parts of tire

--- Problems/TireProblem.py
+++ Problems/TireProblem.py
@@ -1,10 +1,27 @@
+from Domains.Domain import Domain
 from Model.Predicate import Predicate
-from Model.State import State
 from Problems.Problem import Problem
+from Model.State import State
+from Model.Entity import Entity
 
 
-class TireProblem(Problem):
-    def __init__(self, domain):
-        # Initialize a TireProblem object with a specified domain
+class Tire(Problem):
+    def __init__(self, domain: Domain):
         super().__init__(domain)
-        pass
+        flat = Entity("Flat", "Tire")
+        spare = Entity("Spare", "Tire")
+        axle = Entity("Axle", "Location")
+        trunk = Entity("Trunk", "Location")
+
+        at_flat_axle = Predicate("At", [flat, axle])
+
+        at_spare_trunk = Predicate("At", [spare, trunk])
+
+        self.initial_state = State(
+            "",
+            [at_flat_axle, at_spare_trunk],
+        )
+
+        at_spare_axel = Predicate("At", [spare, axle])
+
+        self.goal_state = State("", [at_spare_axel])
